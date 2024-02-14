36ff041498bd444d497f43225620f4762b0ef4db
Bernard Kužnik <bernard.kuznik@gmail.com>
2024-02-14 10:46:42 +0100

    Added text to track progress/errors

--- js/logic.js
+++ js/logic.js
@@ -13,13 +13,26 @@ function checkUrl(url){
 async function downloadVideo(url){
 
   if(checkUrl(url) == false){
-    document.getElementById("error").className = "error-true";
+    document.getElementById("output").className = "red";
+    document.getElementById("output").innerText = "The URL is invalid";
     return false;
   }
-  document.getElementById("error").className = "error-false";
+  document.getElementById("output").className = "white";
+  document.getElementById("output").innerText = "Downloading...";
 
-  response = await fetch("http://127.0.0.1:8000/download?url="+url);
-
-  console.log(response);
+  fetch("http://127.0.0.1:8000/download?url="+url)
+  .then(response => response.json())
+  .then(data => {
+    if(data.message == "Download was successful"){
+      document.getElementById("output").className = "green";
+    }
+    else{
+      document.getElementById("output").className = "red";
+    }
+      document.getElementById("output").innerText = data.message;
+  })
+  .catch(error => {
+    console.log("Error", error);
+  })
   
 }
